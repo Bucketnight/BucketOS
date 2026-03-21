46b174cf2629f76d3f392a8600a43a91c47d0154
Bucketnight <huynhhuu254@gmail.com>
2026-03-21 17:31:47 +0700

    simplest keyboard driver

---
+++ src/kernel/keyboard.c
@@ -0,0 +1,28 @@
+#include "bucketkernel/keyboard.h"
+#include "bucketkernel/ports.h"
+#include "bucketkernel/shell.h"
+
+static const char g_scancode_map[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0,
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z',
+    'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+void keyboard_initialize(void) {
+}
+
+void keyboard_handle_irq(void) {
+    const uint8_t scancode = inb(0x60);
+    if ((scancode & 0x80u) != 0) {
+        return;
+    }
+
+    const char c = g_scancode_map[scancode];
+    if (c != 0) {
+        shell_handle_char(c);
+    }
+}
