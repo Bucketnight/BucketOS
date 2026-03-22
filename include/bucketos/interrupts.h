cbde78f10a0d580b3805061fda3c558ec4412f0d
Bucketnight <huynhhuu254@gmail.com>
2026-03-22 16:23:45 +0700

    Fixed typo

--- include/bucketos/interrupts.h
+++ include/bucketos/interrupts.h
@@ -1,7 +1,7 @@
 #ifndef BUCKETKERNEL_INTERRUPTS_H
 #define BUCKETKERNEL_INTERRUPTS_H
 
-#include "bucketkernel/idt.h"
+#include "bucketos/idt.h"
 
 void interrupts_initialize(void);
 void interrupt_dispatch(registers_t *regs);
