ee25f645f034216e9b70268556bea97403ded587
Bucketnight <huynhhuu254@gmail.com>
2026-03-22 16:05:51 +0700

    Update hypervisor detections in kernel core

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,6 +6,7 @@
 #include "bucketkernel/print.h"
 #include "bucketkernel/shell.h"
 #include "bucketkernel/terminal.h"
+#include "bucketkernel/hypervisor.h"
 
 void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info_addr) {
     terminal_initialize();
@@ -20,6 +21,15 @@ void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info_addr) {
 
     print_line("multiboot: ok");
     memory_initialize((const multiboot_info_t *)(uintptr_t)multiboot_info_addr);
+
+    hypervisor_info_t hv = hypervisor_detect();
+    if (!hv.present) {
+        print_line("hypervisor: bare metal");
+    } else {
+        print_string("hypervisor: ");
+        print_line(hv.vendor);
+    }
+
     interrupts_initialize();
     shell_initialize();
 
@@ -31,3 +41,4 @@ void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info_addr) {
         cpu_halt();
     }
 }
+
