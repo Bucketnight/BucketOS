3e19ce02a8895930318b2bb4d0d608eb46f175e4
Bucketnight <huynhhuu254@gmail.com>
2026-03-22 16:01:44 +0700

    hypervisor detection API

---
+++ include/bucketos/hypervisor.h
@@ -0,0 +1,13 @@
+#ifndef BUCKETKERNEL_HYPERVISOR_H
+#define BUCKETKERNEL_HYPERVISOR_H
+
+#include "bucketkernel/common.h"
+
+typedef struct {
+    bool present;
+    char vendor[13];
+} hypervisor_info_t;
+
+hypervisor_info_t hypervisor_detect(void);
+
+#endif
