0834d8852ed1df3d3d5e2944b24cd1f5776fda70
Bucketnight <huynhhuu254@gmail.com>
2026-03-22 16:24:43 +0700

    Fixed typo

--- include/bucketos/kernel.h
+++ include/bucketos/kernel.h
@@ -1,7 +1,7 @@
 #ifndef BUCKETKERNEL_KERNEL_H
 #define BUCKETKERNEL_KERNEL_H
 
-#include "buckets/common.h"
+#include "bucketos/common.h"
 
 void kernel_main(uint32_t multiboot_magic, uint32_t multiboot_info_addr);
 
