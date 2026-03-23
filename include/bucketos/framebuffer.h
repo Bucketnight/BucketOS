088ea4dbeed93fc5f2821029a97b57a829d1ef78
Bucketnight <huynhhuu254@gmail.com>
2026-03-23 14:01:10 +0700

    Create framebuffer.h

---
+++ include/bucketos/framebuffer.h
@@ -0,0 +1,20 @@
+#ifndef BUCKETOS_FRAMEBUFFER_H
+#define BUCKETOS_FRAMEBUFFER_H
+
+#include "bucketos/common.h"
+#include "bucketos/multiboot.h"
+
+typedef struct {
+    bool available;
+    uintptr_t address;
+    uint32_t pitch;
+    uint32_t width;
+    uint32_t height;
+    uint8_t bpp;
+    uint8_t type;
+} framebuffer_info_t;
+
+void framebuffer_initialize(const multiboot_info_t *mbi);
+const framebuffer_info_t *framebuffer_info(void);
+
+#endif
