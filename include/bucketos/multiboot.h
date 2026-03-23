617ea86c513762ea7ac4bca1c6e7c6d3d8716af4
Bucketnight <huynhhuu254@gmail.com>
2026-03-23 14:21:28 +0700

    Update multiboot.h

--- include/bucketos/multiboot.h
+++ include/bucketos/multiboot.h
@@ -5,6 +5,7 @@
 
 #define MULTIBOOT_BOOTLOADER_MAGIC 0x2BADB002u
 #define MULTIBOOT_INFO_MEM_MAP      0x00000040u
+#define MULTIBOOT_INFO_FRAMEBUFFER 0x00001000u
 
 typedef struct {
     uint32_t flags;
@@ -17,6 +18,25 @@ typedef struct {
     uint8_t syms[16];
     uint32_t mmap_length;
     uint32_t mmap_addr;
+    uint32_t drives_length;
+    uint32_t drives_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t framebuffer_bpp;
+    uint8_t framebuffer_type;
+    uint16_t reserved;
 } __attribute__((packed)) multiboot_info_t;
 
 typedef struct {
