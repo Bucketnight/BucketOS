4cbbf01f67f08aa5ed6b4b777e1c150995efee37
Bucketnight <huynhhuu254@gmail.com>
2026-03-24 15:40:09 +0700

    Update shell.h

--- include/bucketos/shell.h
+++ include/bucketos/shell.h
@@ -4,5 +4,8 @@
 void shell_initialize(void);
 void shell_prompt(void);
 void shell_handle_char(char c);
+bool shell_has_pending_command(void);
+void shell_run_pending_command(void);
+void shell_request_stop(void);
 
 #endif
