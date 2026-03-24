292d759ab8c293d835d3d171bc2ccddd3dcba461
Bucketnight <huynhhuu254@gmail.com>
2026-03-24 15:32:46 +0700

    Create vfs.h

---
+++ include/bucketos/vfs.h
@@ -0,0 +1,17 @@
+#ifndef BUCKETOS_VFS_H
+#define BUCKETOS_VFS_H
+
+#include "bucketos/common.h"
+
+typedef void (*vfs_list_callback_t)(const char *name, bool is_dir, void *context);
+
+void vfs_initialize(void);
+void vfs_load_initrd(const void *address, size_t size);
+bool vfs_list(const char *path, vfs_list_callback_t callback, void *context);
+bool vfs_is_dir(const char *path);
+bool vfs_read(const char *path, const char **data, size_t *size);
+bool vfs_mkdir(const char *path);
+bool vfs_touch(const char *path);
+bool vfs_write(const char *path, const char *data);
+
+#endif
