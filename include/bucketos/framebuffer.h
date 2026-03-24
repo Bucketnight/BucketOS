bead9552fccaa400709bbe04bbc99f69a02f84ac
Bucketnight <huynhhuu254@gmail.com>
2026-03-24 15:39:39 +0700

    Update framebuffer.h

--- include/bucketos/framebuffer.h
+++ include/bucketos/framebuffer.h
@@ -18,5 +18,7 @@ void framebuffer_initialize(const multiboot_info_t *mbi);
 const framebuffer_info_t *framebuffer_info(void);
 void framebuffer_put_pixel(uint32_t x, uint32_t y, uint32_t color);
 void framebuffer_fill(uint32_t color);
+void framebuffer_draw_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color);
+
 
 #endif
