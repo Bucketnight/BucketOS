1669f383fc83a10f62c0fbecf29d238b5b76ab9d
Bucketnight <huynhhuu254@gmail.com>
2026-03-25 13:00:38 +0700

    Create paging.h

---
+++ include/bucketos/paging.h
@@ -0,0 +1,20 @@
+#ifndef BUCKETOS_PAGING_H
+#define BUCKETOS_PAGING_H
+
+#include "bucketos/common.h"
+#include "bucketos/framebuffer.h"
+
+typedef struct {
+    uintptr_t code_virtual;
+    uintptr_t code_physical;
+    uintptr_t stack_bottom_virtual;
+    uintptr_t stack_top_virtual;
+    uintptr_t stack_physical;
+} user_space_mapping_t;
+
+void paging_initialize(const framebuffer_info_t *framebuffer);
+bool paging_is_enabled(void);
+const user_space_mapping_t *paging_user_space(void);
+void paging_map_initial_user_space(void);
+
+#endif
