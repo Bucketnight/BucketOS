13aadebefb8017bf0013f5f7cb08e3b9d7ef2815
Bucketnight <huynhhuu254@gmail.com>
2026-03-29 14:17:26 +0700

    Update keyboard.h

--- include/bucketos/keyboard.h
+++ include/bucketos/keyboard.h
@@ -1,3 +1,12 @@
+// keyboard.h: PS/2 keyboard driver API.
+
+/*
+ * Reading guide:
+ * - Purpose: keyboard.h: PS/2 keyboard driver API.
+ * - Start reading at: keyboard_initialize
+ * - Tip: This is part of the public API; keep it stable and document any assumptions.
+ */
+
 #ifndef BUCKETOS_KEYBOARD_H
 #define BUCKETOS_KEYBOARD_H
 
