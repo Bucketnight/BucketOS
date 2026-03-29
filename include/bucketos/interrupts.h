d7361d6d070252c46a50893fbafa375b1eab5a34
Bucketnight <huynhhuu254@gmail.com>
2026-03-29 14:16:45 +0700

    Update interrupts.h

--- include/bucketos/interrupts.h
+++ include/bucketos/interrupts.h
@@ -1,3 +1,12 @@
+// interrupts.h: Interrupt subsystem API (PIC remap, ISR install, enable/disable).
+
+/*
+ * Reading guide:
+ * - Purpose: interrupts.h: Interrupt subsystem API (PIC remap, ISR install, enable/disable).
+ * - Start reading at: interrupts_initialize
+ * - Tip: This is part of the public API; keep it stable and document any assumptions.
+ */
+
 #ifndef BUCKETOS_INTERRUPTS_H
 #define BUCKETOS_INTERRUPTS_H
 
